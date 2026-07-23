3b8ec6e54e4bfcfa58ef1ed137beb978887aaec2
Florian Moser <florian_moser@gmx.at>
2026-07-23 09:47:38 +0200

    Zeitexa PWA v1.5.0

--- android/app/src/main/kotlin/at/moser/zeitexa/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package at.moser.zeitexa
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-// FlutterFragmentActivity statt FlutterActivity: wird von local_auth
-// (BiometricPrompt) vorausgesetzt.
-class MainActivity : FlutterFragmentActivity()
--- lib/logic/fehlerprotokoll_stub.dart
+++
@@ -1,6 +0,0 @@
-/// Web-Variante: kein Dateisystem. Fehler landen nur in der
-/// Browser-Konsole (über den Standard-Handler), es wird keine Datei
-/// geschrieben. Siehe lib/logic/fehlerprotokoll.dart.
-Future<void> schreibeFehler(String eintrag) async {}
-
-Future<String?> fehlerprotokollPfad() async => null;
