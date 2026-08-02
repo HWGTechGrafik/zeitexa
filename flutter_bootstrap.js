d09ced05f410412e872572abb827af9e43ea2a58
Florian Moser <florian_moser@gmx.at>
2026-08-02 23:40:49 +0200

    Zeitexa PWA v1.13.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "638080984" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "872159125" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
