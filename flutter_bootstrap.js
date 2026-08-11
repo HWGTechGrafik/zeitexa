72217462e2aa6cf561af31229e7bbeb64c5653fb
Florian Moser <florian_moser@gmx.at>
2026-08-11 15:54:54 +0200

    Zeitexa PWA v1.15.0 (Konten-Karte breiter, Uebersicht ein- und ausklappbar)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1482320112" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2927359355" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
